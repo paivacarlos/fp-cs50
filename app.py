9d0695badd1019ce7051e7eded950391c17166f1
Carlos Paiva <paivakk22@gmail.com>
2026-06-05 18:35:51 -0300

    create a main page to be redirect

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from routes.auth import auth_bp
+from routes.main import main_bp
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,3 +11,4 @@ app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "key-secret-develop-env")
 
 app.register_blueprint(auth_bp)
+app.register_blueprint(main_bp)
